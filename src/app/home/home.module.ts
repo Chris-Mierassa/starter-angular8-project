283f17aa9433e8218dc67c3835743d3209bb5645
unknown <chris.mierassa@gmail.com>
2019-08-17 14:34:53 +0200

    ends lazy loading and add  nav

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import {HomeComponent} from './home.component';
+
+
+@NgModule({
+  declarations: [HomeComponent, NavComponent, FooterComponent],
+  exports: [
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
