ba89317bec8ab1dde464b77073432b3f9f564aa5
unknown <chris.mierassa@gmail.com>
2019-08-17 21:43:43 +0200

    ajout du header et dun corp

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,10 +5,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import {HomeComponent} from './home.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, NavComponent, FooterComponent],
+  declarations: [HomeComponent, NavComponent, FooterComponent, HeaderComponent],
   exports: [
     NavComponent,
     FooterComponent
