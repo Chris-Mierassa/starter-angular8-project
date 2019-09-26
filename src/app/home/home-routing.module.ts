13dcb74b0730e0ca716a51c9502ab55d31b7477c
Chris Mierassa <cmierassa-ext@siplec.com>
2019-09-26 11:25:43 +0200

    add translate

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home.component';
-
+import {IndexComponent} from './index/index.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    children: [{path: '', component: IndexComponent}]
   }
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,17 +6,19 @@ import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import {HomeComponent} from './home.component';
 import { HeaderComponent } from './header/header.component';
-
+import { IndexComponent } from './index/index.component';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [HomeComponent, NavComponent, FooterComponent, HeaderComponent],
+  declarations: [HomeComponent, NavComponent, FooterComponent, HeaderComponent, IndexComponent],
   exports: [
     NavComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    TranslateModule
   ]
 })
 export class HomeModule { }
